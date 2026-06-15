bd515a4869bb2683aaf4c1b24df181a3e5d1042c
Drew Holland <drexh47127@gmail.com>
2026-06-15 14:17:36 -0400

    Add user input multiplication function

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__=="__main__":
-    print("hello world")
+def multiply_two_numbers_from_input():
+    first = float(input("Enter the first number: "))
+    second = float(input("Enter the second number: "))
+    product = first * second
+    print(f"Product: {product}")
+
+
+if __name__ == "__main__":
+    multiply_two_numbers_from_input()
