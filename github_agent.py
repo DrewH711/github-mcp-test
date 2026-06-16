9502ab0159901495f464bd4f5e0a67060bbbb2b4
Drew Holland <drexh47127@gmail.com>
2026-06-16 14:26:39 -0400

    First commit of agent setup

---
+++ github_agent.py
@@ -0,0 +1,31 @@
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPToolset
+import dotenv
+import utils
+from os import getenv
+
+dotenv.load_dotenv("keys.env")
+
+model = utils.load_model("kimi-k2.6")
+
+githubMCP = MCPToolset(client="https://api.githubcopilot.com/mcp/", auth=getenv("GITHUB_PAT"))
+
+allowed_tool_names = []
+
+allowed_tools = githubMCP.filtered(lambda 
+ctx, tool: tool.name in allowed_tool_names)
+
+github_agent = Agent(
+    model=model,
+    output_type=str,
+    instructions="Be helpful and concise. When making commits, pull requests, or comments, always include the following: 'This [commit/PR/comment] was written by [LLM MODEL NAME] on behalf of [USER NAME], and may include mistakes'",
+    toolsets=[allowed_tools]
+)
+
+prompt = ""
+
+while True:
+
+    output = github_agent.run_sync(user_prompt=prompt).output
+
+    print(output)
